df037ade28440aedb22d77f9958e9ce3b3e3c4be
lrlna <shestak.irina@gmail.com>
2019-01-24 12:01:16 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import * as wasm from "hello-wasm-pack";
+import("mongodb-schema-parser")
+  .then((module) => {
+    console.log(module)
+    const schemaParser = new module.SchemaParser
 
-wasm.greet();
+    // fetch('./fanclub.json')
+    //   .then(response => response.text())
+    //   .then(data => {
+    //     var now = performance.now()
+    //     var json = data.split("\n")
+    //     for (var i = 0; i < json.length; i++) {
+    //       if (json[i] !== '') {
+    //         schemaParser.write(json[i])
+    //       }
+    //     }
+    //     var state = schemaParser.toJson()
+    //     console.log("time took", performance.now() - now)
+    //     console.log(state)
+    //   })
+  })
+  .catch(e => console.log("error loading module", e))
+;
+
